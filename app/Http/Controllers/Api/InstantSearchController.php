0ca5fec6b263ba6c7eecea2ca809bd71124526ba
Akhmad Jainudin Noor Rahman <akhmadjainudinnr@gmail.com>
2025-11-13 22:45:50 +0800

    feat: Menambahkan controller untuk Instant Search

---
+++ app/Http/Controllers/Api/InstantSearchController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\ProductRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class InstantSearchController extends Controller
+{
+    protected ProductRepository $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): JsonResponse
+    {
+        $query = $request->query('q');
+        if (empty($query)) {
+            return response()->json([]);
+        }
+
+        $result = $this->productRepository->getInstanceSearchResult($query, 8);
+        return response()->json($result);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+
+    }
+}
